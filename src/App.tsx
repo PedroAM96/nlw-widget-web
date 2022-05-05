97430906872bd1df83ecea87be6cffbbc5fa5eda
pedro.angelo <pedromarques08@hotmail.com>
2022-05-05 18:26:24 -0300

    app responsive html

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,43 @@
 import { Widget } from './components/Widget'
 
 export function App() {
-  return <Widget />
+  return (
+    <>
+      <div className='flex flex-row justify-between w-100vw h-16 px-2 bg-zinc-800 sd:px-24'>
+        <div className='w-32 m-3 rounded bg-zinc-700'></div>
+        <div className='hidden md:flex'>
+          <div className='w-24 my-auto mx-2 h-3 rounded bg-zinc-700'></div>
+          <div className='w-24 my-auto mx-2 h-3 rounded bg-zinc-700'></div>
+          <div className='w-24 my-auto mx-2 h-3 rounded bg-zinc-700'></div>
+          <div className='w-24 my-auto mx-2 h-3 rounded bg-zinc-700'></div>
+        </div>
+        <div className='hidden md:flex place-items-center justify-between'>
+          <div className='w-6 h-6 mx-2 rounded bg-zinc-700'></div>
+          <div className='w-6 h-6 mx-2 rounded bg-zinc-700'></div>
+          <div className='w-6 h-6 mx-2 rounded-full bg-zinc-700'></div>
+        </div>
+        <div className='flex flex-col md:hidden my-auto'>
+          <div className='w-4 my-0.5 mx-2 h-1 rounded bg-zinc-700'></div>
+          <div className='w-4 my-0.5 mx-2 h-1 rounded bg-zinc-700'></div>
+          <div className='w-4 my-0.5 mx-2 h-1 rounded bg-zinc-700'></div>
+        </div>
+      </div>
+      <section>
+        <main className='mobile-main md:mx-10 md:my-10' >
+          <div className='md:mx-auto md:w-[70vw] bg-zinc-800 h-24  rounded-md' >
+            <p className='text-zinc-400 p-8'>
+              Experimente enviar um feedback de um bug na aplicação 🐛 
+            </p>
+          </div>
+          <div className='mobile-cols grid md:grid-cols-3 md:gap-6 grid-flow-row-dense w-[70vw] mx-auto my-6' >
+            <div className='bg-zinc-800 h-48 rounded-md' ></div>
+            <div className='bg-zinc-800 h-48 md:col-span-2 rounded-md' ></div>
+            <div className='bg-zinc-800 h-48 md:col-span-2 rounded-md' ></div>
+            <div className='bg-zinc-800 h-48 rounded-md' ></div>
+          </div>
+        </main>
+      </section>
+      <Widget />
+    </>
+  )
 }
--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,10 +1,11 @@
 import { ChatTeardropDots } from 'phosphor-react';
 import { Popover } from '@headlessui/react'
+import { WidgetForm } from './WidgetForm';
 
 export function Widget() {
   return (
-    <Popover className='absolute bottom-5 right-5'>
-      <Popover.Panel>Hello world</Popover.Panel>
+    <Popover className='absolute flex flex-col items-end bottom-5 right-5 md:bottom-8 md:right-8'>
+      <Popover.Panel><WidgetForm></WidgetForm></Popover.Panel>
       <Popover.Button className='flex items-center bg-brand-500 rounded-full px-3 h-12 text-white group'>
         <ChatTeardropDots className='w-6 h-6 ' />
         <span className='max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear'> 
