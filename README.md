e2b04a7108bced8a836158e7c8c918e65656b1d4
pedro.angelo <pedromarques08@hotmail.com>
2022-05-29 16:57:57 -0300

    ending development and creating a readme

---
+++ README.md
@@ -0,0 +1,32 @@
+<p align="center">
+  <img src="./src/assets/wallpaper/wallpaper_1400x900.svg" width="50%"/>
+</p>
+
+# **NLW RETURN IMPULSE**
+
+## **Description**
+
+Widget developed with _React_ to collect user feedback while browsing a platform or website.
+
+## **Install**
+
+```bash
+# primeiros passos
+$ npm install
+```
+
+- Create the _.env.local_ file according to the template below
+
+```bash
+#app
+VITE_API_URL=
+```
+
+## **Usage**
+
+```bash
+# development
+$ npm run dev
+```
+
+- In the feedback button, set the type and leave a comment about the problem that happened to you
--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,7 +1,9 @@
 import { ArrowLeft } from 'phosphor-react';
 import { FormEvent, useState } from 'react';
 import { FeedbackType, feedbackTypes } from '..';
+import { api } from '../../../lib/api';
 import { CloseButton } from '../../CloseButton';
+import { Loading } from '../../Loading';
 import { ScreenshotButton } from '../ScreenshotButton';
 
 interface FeedbackContentStepProps {
@@ -17,13 +19,20 @@ export function FeedbackContentStep({
 }: FeedbackContentStepProps) {
   const [screenshot, setScreenshot] = useState<string | null>(null);
   const [comment, setComment] = useState<string>('');
+  const [isSendingFeedback, setIsSendingFeedback] = useState<boolean>(false);
+
   const feedbackTypeInfo = feedbackTypes[feedbackType];
-  function handleSubmitFeedback(event: FormEvent) {
+  async function handleSubmitFeedback(event: FormEvent) {
     event.preventDefault();
-    console.log({
-      screenshot,
+    setIsSendingFeedback(true);
+
+    await api.post('/feedbacks', {
+      type: feedbackType,
       comment,
+      screenshot,
     });
+
+    setIsSendingFeedback(false);
     onFeedbackSent();
   }
   return (
@@ -65,9 +74,9 @@ export function FeedbackContentStep({
           <button
             type='submit'
             className='p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500'
-            disabled={comment.length === 0}
+            disabled={comment.length === 0 || isSendingFeedback}
           >
-            Enviar feedback
+            {isSendingFeedback ? <Loading /> : 'Enviar feedback'}
           </button>
         </footer>
       </form>
